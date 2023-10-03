ae00fafdf8ced645e90f30ead5a0f472bd462740
TheMiniTom Bury <theminitom59@hotmail.com>
2023-10-03 09:43:02 +0200

    fin tp1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -35,6 +36,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   int currentPageIndex = 0;
+  int index = 0;
+  int _valeur = 0;
 
   void _incrementCounter() {
     setState(() {
@@ -54,6 +57,13 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _randomValeur() {
+    setState(() {
+      Random random = new Random();
+      _valeur = random.nextInt(_counter);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,29 +71,61 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(
-              'images/logo_facebook.png',
-              width: 100,
-              height: 100,
-            ),
-            Image.network(
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Jean_Lassalle_04_%28cropped%29.jpg/1200px-Jean_Lassalle_04_%28cropped%29.jpg",
-                width: 150,
-                height: 150),
-            const Text(
-              "Nombres de Follower",
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: <Widget>[
+        Container(
+          child: ListView(
+            children: <Widget>[
+              Image.asset(
+                'images/logo_facebook.png',
+                width: 100,
+                height: 100,
+              ),
+              Image.network(
+                  "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Jean_Lassalle_04_%28cropped%29.jpg/1200px-Jean_Lassalle_04_%28cropped%29.jpg",
+                  width: 150,
+                  height: 150),
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "Nombres de Follower",
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
+        Container(
+            color: Colors.green,
+            alignment: Alignment.center,
+            child: ListView(children: [
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  '$_valeur',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all<Color>(
+                      const Color.fromARGB(255, 0, 0, 0)),
+                ),
+                onPressed: () => {_randomValeur()},
+                child: const Text('Générer un nombre aléatoire'),
+              )
+            ])),
+        Container(
+          color: Color.fromARGB(255, 50, 34, 190),
+          alignment: Alignment.center,
+          child: const Text("Parametre"),
+        ),
+      ][currentPageIndex],
       floatingActionButton: Stack(
         children: <Widget>[
           Padding(
@@ -112,27 +154,23 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Theme.of(context)
-            .colorScheme
-            .primary, // Adjust the color as needed.
-        unselectedItemColor: Colors.grey, // Adjust the color as needed.
-        currentIndex: currentPageIndex,
-        onTap: (int index) {
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
           setState(() {
             currentPageIndex = index;
-          });
+          }); // Adjust the color as needed.
         },
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
             icon: Icon(Icons.home),
             label: 'Accueil',
           ),
-          BottomNavigationBarItem(
+          NavigationDestination(
             icon: Icon(Icons.apps),
-            label: 'Page suivante',
+            label: 'Générateur',
           ),
-          BottomNavigationBarItem(
+          NavigationDestination(
             icon: Icon(Icons.bookmark),
             label: 'Parametre',
           ),
